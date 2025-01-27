2792cd583f302cdbc75bcd6a10d68a98149abe7f
Disidrose <19365192+Disidrose@users.noreply.github.com>
2025-01-27 19:56:03 -0300

    User Input
    Implemented user input by creating a function

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ function getComputerChoice() {
     return options_rpc[Math.floor(Math.random() * options_rpc.length)];
 }
 
+function getHumanChoice() {
+    let userInput = prompt("Choose between Rock, Paper or Scissors: ").toLowerCase();
+    let userInputCap = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+    switch (userInputCap) {
+        case "Rock":
+        case "Paper":
+        case "Scissors":
+            console.log(`User input: ${userInputCap}`);
+            break;
+        default:
+            console.log("Error: Please enter 'Rock', 'Paper' or 'Scissors'.")
+    }
+}
+
 console.log(getComputerChoice());
+getHumanChoice();
